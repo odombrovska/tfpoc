907c55c4d2129f40d84e31e59606fb8c631c83e2
ssi_okuzina@qbsol.com <ssi_okuzina@qbsol.com>
2021-06-25 21:14:52 +0300

    adding winrar installation script to the files

--- scripts/installWinUpdates.ps1
+++
@@ -1,7 +0,0 @@
-Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force
-Import-PackageProvider -Name NuGet -RequiredVersion 2.8.5.20
-
-Install-Module PSWindowsUpdate
-Import-Module PSWindowsUpdate
-
-Install-WindowsUpdate -AcceptAll -MicrosoftUpdate -AutoReboot
